6c3bfddd555bf7df464d0c9075e5229de52046ae
ThangaPrabha03 <thangaprabha362005@gmail.com>
2025-07-31 18:34:36 +0530

    first commit

--- models/Event.js
+++ models/Event.js
@@ -1,22 +1,10 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-const EventSchema = new mongoose.Schema({
-  title: String,
-  date: Date,
-  description: String,
-  location: String,
-});
-
-module.exports = mongoose.model('Event', EventSchema);
-=======
-
 const eventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   date: { type: Date, required: true },
   description: String,
+  location: String
 }, { timestamps: true });
 
-
 module.exports = mongoose.model('Event', eventSchema);
->>>>>>> f84c02b2a5467963b47c160b5dee3f873701f94a
